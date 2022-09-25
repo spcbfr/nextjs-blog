831aef912378c94357858f41e393ed33246509e2
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-09-25 11:59:14 +0100

    finishing the navbar on desktop

--- components/navbar.js
+++ components/navbar.js
@@ -4,15 +4,24 @@ import ProfilePic from "/public/me.png";
 
 export default function Navbar() {
   return (
-    <nav className=" bg-slate-600/30 flex  top-10 backdrop-blur p-2 rounded-2xl fixed ">
-      <Link href="/">
-        <Image
-          src={ProfilePic}
-          width="40"
-          height="40"
-          className="rounded-full cursor-pointer"
-        />
-      </Link>
+    <nav className=" bg-slate-600/30 flex justify-between items-center px-3   top-10 backdrop-blur p-2 rounded-2xl sticky">
+      <div className="transition hover:scale-110 relative top-1">
+        <Link href="/">
+          <Image
+            src={ProfilePic}
+            width="40"
+            height="40"
+            className="rounded-full cursor-pointer"
+          />
+        </Link>
+      </div>
+      <a
+        className="font-semibold"
+        target={"_blank"}
+        href="https://twitter.com/spacebuffer"
+      >
+        Twitter
+      </a>
     </nav>
   );
 }
