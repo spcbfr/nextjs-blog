3f56f83adea52dffcfef1117a908d6efdd1ecb03
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-10-08 14:00:32 +0100

    Add a favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Blaka&family=Poppins:wght@400;500;600;800&display=swap"
           rel="stylesheet"
