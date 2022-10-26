fa9d3606175a2f93a428371f24e5ff9c0750748a
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-10-26 20:44:41 +0100

    Fix Attributes for React

--- pages/index.js
+++ pages/index.js
@@ -49,9 +49,9 @@ export default function Home({ allPostsData }) {
               <path
                 d="M128,88c0-22,18.5-40.3,40.5-40a40,40,0,0,1,36.2,24H240l-32.3,32.3A127.9,127.9,0,0,1,80,224c-32,0-40-12-40-12s32-12,48-36c0,0-64-32-48-120,0,0,40,40,88,48Z"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></path>
             </svg>
           </a>
@@ -71,37 +71,37 @@ export default function Home({ allPostsData }) {
               <path
                 d="M84,240a23.9,23.9,0,0,0,24-24V168"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></path>
               <path
                 d="M172,240a23.9,23.9,0,0,1-24-24V168"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></path>
               <path
                 d="M152,168h16a23.9,23.9,0,0,1,24,24v8a23.9,23.9,0,0,0,24,24"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></path>
               <path
                 d="M104,168H88a23.9,23.9,0,0,0-24,24v8a23.9,23.9,0,0,1-24,24"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></path>
               <path
                 d="M111.8,64A52,52,0,0,0,68,40a52,52,0,0,0-3.5,44.7A49.3,49.3,0,0,0,56,112v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.3,49.3,0,0,0-8.5-27.3A52,52,0,0,0,188,40a52,52,0,0,0-43.8,24Z"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></path>
             </svg>
           </a>
@@ -123,8 +123,8 @@ export default function Home({ allPostsData }) {
                 cy="128"
                 r="40"
                 fill="none"
-                stroke-miterlimit="10"
-                stroke-width="16"
+                strokeMiterlimit="10"
+                strokeWidth="16"
               ></circle>
               <rect
                 x="36"
@@ -133,9 +133,9 @@ export default function Home({ allPostsData }) {
                 height="184"
                 rx="48"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="16"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               ></rect>
               <circle cx="180" cy="76" r="12"></circle>
             </svg>
