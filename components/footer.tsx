80f425063feb0f6200d6fe53679cfd7e714dbe5d
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-11-08 07:03:13 +0100

    Migrate the footer to the new next/link

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer className="  bg-zinc-900 text-zinc-400 mt-3 print:hidden text-sm text-center  py-4">
       Built with NextJS and Contentlayer •{" "}
-      <Link href="/rss.xml">
-        <a className="underline font-medium decoration-zinc-400">
+      <Link href="/rss.xml" className="underline font-medium decoration-zinc-400">
+        
           Subscribe to RSS
-        </a>
+        
       </Link>
     </footer>
   );
