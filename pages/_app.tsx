ca0b398c2e3fc0e0d359da978c69c7cba46e95d8
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-11-09 20:34:04 +0100

    Fix Prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,10 @@ import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-
-  <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
