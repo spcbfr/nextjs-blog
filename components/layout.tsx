261adf5761dc27ad5023b53d0546ce765a483a20
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-11-15 07:29:07 +0100

    Add Mastodon Link

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from "react";
+import Link from "next/link";
+
 interface Props {
   children?: ReactNode;
   // any props that come into the component
@@ -8,6 +10,12 @@ export default function Layout({ children }: Props) {
     <>
       <main className="relative z-10 mb-5 mt-12 grid grid-cols-[1fr,min(640px,100%),1fr] xl:gap-x-3 gap-y-5  px-7 sm:mt-32 sm:px-0 xl:grid-cols-[1fr,minmax(auto,240px),min(640px,100%),minmax(auto,240px),1fr] [&>*]:col-start-2 xl:[&>*]:col-start-3 ">
         {children}
+        <footer className="font-display text-center mt-2 [&>a]:text-yellow-600 [&>a]:font-bold">
+          Subscribe to <Link href="/rss.xml">RSS</Link> • Follow me on{" "}
+          <a rel="me" href="https://fosstodon.org/@spacebuffer">
+            Mastodon
+          </a>
+        </footer>
       </main>
     </>
   );
