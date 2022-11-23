1260a2c31943f9ec6e4ef16d2d0ab07a4441d414
Youssef Bouzekri <youssefbouzekri@protonmail.com>
2022-11-23 20:04:33 +0100

    Remove sparkles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        newSpin: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(180deg)" },
+        },
+        growAndShrink: {
+          "0%": { transform: "scale(0)" },
+          "50%": { transform: "scale(1)" },
+          "100%": { transform: "scale(0)" },
+        },
+      },
+      animation: {
+        spiner: "newSpin 600ms linear forwards",
+        grower: "growAndShrink 600ms ease-in-out forwards",
+      },
       fontFamily: {
         sans: ["var(--font-karla)"],
         display: ["Space Grotesk"],
