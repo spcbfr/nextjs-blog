a44fa8b2d6215aedfa6cb0518398ef194a616861
Yusuf Bouzekri <youssefbouzekri@protonmail.com>
2022-12-23 20:51:43 +0100

    Fix typescript error

--- components/enter.tsx
+++ components/enter.tsx
@@ -1,5 +1,5 @@
-
-export default function Enter(props) {
+import React from "react"
+export default function Enter(props: any) {
     return (
         <div className="text-center font-display font-bold text-xl">{props.children}</div>
     )
