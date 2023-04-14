4ad8fc180ca67fb875a14450ba5141b9ef2eb1cb
Yusuf Bouzekri <youssefbouzekri@protonmail.com>
2023-04-14 14:50:44 +0100

    Update to next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
-import { Karla } from "@next/font/google";
+import { Karla } from "next/font/google";
 
 const karla = Karla({
   variable: "--font-karla",
