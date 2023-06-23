16cffdfe9f3162495e1c36604680aa6a58a2e99d
Yusuf Bouzekri <youssefbouzekri@protonmail.com>
2023-06-23 16:04:51 +0100

    Update loading state for spotify widget

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -1,5 +1,6 @@
 import { currentlyPlayingSong } from "lib/spotify";
 import { NextRequest, NextResponse } from "next/server";
+import { Spotify } from "./types";
 export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
   const response = await currentlyPlayingSong();
@@ -8,7 +9,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ isPlaying: false }, {status: 200});
   }
 
-  const song = await response.json();
+  const song: Spotify = await response.json();
 
   if (song.item === null) {
     return NextResponse.json({ isPlaying: false }, {status: 200});
@@ -16,7 +17,7 @@ export async function GET(request: NextRequest) {
 
   const isPlaying = song.is_playing;
   const title = song.item.name;
-  const artist = song.item.artists.map((_artist : {name: string}) => _artist.name).join(", ");
+  const artist = song.item.artists.slice(0,2).map((_artist : {name: string}) => _artist.name).join(", ");
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
--- components/spotify-playing.tsx
+++ components/spotify-playing.tsx
@@ -21,23 +21,46 @@ function SpotifyWrapper({children}: {children: ReactNode}){
 }
 export default function Spotify(){
     const {data, error, isLoading}: swr  = useSWR("/api/spotify", fetcher)
-    if (error) return <SpotifyWrapper>failed to load</SpotifyWrapper>
-    if (isLoading) return <SpotifyWrapper>loading...</SpotifyWrapper>
+    if (error) {
+      return (
+          <SpotifyWrapper >
+            <div style={{backgroundImage: "url('/spotify.jpg')"}} className="md:w-12 md:h-12 h-10 w-10 bg-cover rounded-lg" />
+            <div className="inline ms-2">
+              <h1 className="text-left">Failed to load</h1>
+              <h2 className="text-left font-medium sm:text-base text-sm">Something went wrong</h2>
+            </div>
+          </SpotifyWrapper>)
+    }
+    if (isLoading) { 
+      return (
+        <div className="bg-emerald-900 rounded-lg p-2 flex flex-row">
+          <div className="md:h-12 md:w-12 h-10 w-10 rounded-lg animate-pulse bg-emerald-600"></div>
+          <div className="space-y-3 mt-[2px]">
+            <div className="w-28 h-[14px] ml-2 rounded-md bg-emerald-600 animate-pulse"></div>
+            <div className="w-12 h-[14px] ml-2 rounded-md bg-emerald-600 animate-pulse"></div>
+          </div>
+        </div>
+      )
+    }
     if(data.isPlaying){
       return (
           <SpotifyWrapper >
-            <div style={{backgroundImage: `url(${data.albumImageUrl})`}}  className="md:w-12 md:h-12 h-10 w-10 bg-cover rounded-md" />
-            <div className="inline ms-2">
+            <div style={{backgroundImage: `url(${data.albumImageUrl})`}}  className="md:w-12 md:h-12 h-10 w-10 bg-cover rounded-lg" />
+            <div className="inline text-left ms-2">
               <h1><a href={data.songUrl} className="" target="_blank">{data.title}</a></h1>
-              <h2 className="text-left font-medium sm:text-base text-sm">{data.artist}</h2>
+              <h2 className="font-medium sm:text-base text-sm">{data.artist}</h2>
             </div>
           </SpotifyWrapper>
+
       )
     } else {
       return (
-          <SpotifyWrapper>
-            <h1 className="font-bold">Not Listening on Spotify</h1>
-          </SpotifyWrapper>
-      )
+          <SpotifyWrapper >
+            <div style={{backgroundImage: "url('/spotify.jpg')"}} className="md:w-12 md:h-12 h-10 w-10 bg-cover rounded-lg" />
+            <div className="inline ms-2">
+              <h1 className="text-left">Not Listening</h1>
+              <h2 className="text-left font-medium sm:text-base text-sm">Yusuf's Spotify</h2>
+            </div>
+          </SpotifyWrapper>)
     }
 }
