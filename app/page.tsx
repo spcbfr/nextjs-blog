ba1ca76076c358c76051221a8da6a0af54528e77
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-08-28 14:44:42 +0100

    New article

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  generateRssFeed();
+  generateRssFeed()
   const sortedPosts = allPosts.sort((a, b) => {
     return Number(new Date(b.date)) - Number(new Date(a.date));
   });
